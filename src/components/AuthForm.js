14ff89f022f571c76134952b22fbaa83da5238e9
Eli-BH <eli.i.henderson95@gmail.com>
2022-02-01 07:15:47 -0500

    SETUP: auth form component

---
+++ src/components/AuthForm.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native";
+import React, { useState } from "react";
+import { Text, Button, Input } from "react-native-elements";
+
+const AuthForm = () => {
+  return (
+    <View>
+      <Text></Text>
+    </View>
+  );
+};
+
+export default AuthForm;
+
+const styles = StyleSheet.create({});
