8444f3ddc861226714db6de9e74b54153f8fbd90
Eli-b-good <eli.i.henderson95@gmail.com>
2022-02-04 00:12:27 -0500

    TEST: image layout

--- src/Screens/HomeScreen.js
+++ src/Screens/HomeScreen.js
@@ -1,8 +1,16 @@
-import { StyleSheet, Text, FlatList } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  FlatList,
+  View,
+  Image,
+  Dimensions,
+} from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { useDispatch, useSelector } from "react-redux";
 import { getHotPosts, hotContentSelector } from "../Redux/hotContent";
 import React, { useEffect } from "react";
+import { Video } from "expo-av";
 
 const HomeScreen = () => {
   const dispatch = useDispatch();
@@ -12,15 +20,50 @@ const HomeScreen = () => {
   useEffect(() => {
     dispatch(getHotPosts());
   }, []);
+  const video = React.useRef();
 
   return (
-    <SafeAreaView forceInset={{ top: "always" }}>
+    <SafeAreaView forceInset={{ top: "always" }} style={styles.container}>
       {loading ? (
         <Text>Loading</Text>
       ) : error ? (
         <Text>{error}</Text>
       ) : (
-        <FlatList />
+        <FlatList
+          data={content}
+          keyExtractor={(item) => item.post.id}
+          renderItem={({ item }) => {
+            return item.type === "image" ? (
+              <View style={styles.itemBox}>
+                <Image
+                  style={styles.image}
+                  source={{
+                    uri: item.post.preview.images[0].source.url.includes(
+                      "https://external"
+                    )
+                      ? item.post.thumbnail
+                      : item.post.preview.images[0].source.url,
+                  }}
+                />
+                <Text style={styles.text}>{item.post.title}</Text>
+              </View>
+            ) : (
+              <View style={styles.itemBox}>
+                <Video
+                  ref={video}
+                  style={styles.video}
+                  source={{
+                    uri: item.post.secure_media.reddit_video.fallback_url,
+                  }}
+                  useNativeControls
+                  resizeMode="contain"
+                  isLooping
+                />
+                <Text style={styles.text}>{item.post.title}</Text>
+              </View>
+            );
+          }}
+        />
       )}
     </SafeAreaView>
   );
@@ -28,4 +71,34 @@ const HomeScreen = () => {
 
 export default HomeScreen;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "#1c1c1c",
+  },
+  itemBox: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "black",
+    borderWidth: 1,
+    borderColor: "#f5f0e4",
+    margin: 4,
+
+    borderRadius: 5,
+  },
+  image: {
+    flex: 1,
+    width: Dimensions.get("window").width,
+    height: 300,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  video: {
+    flex: 1,
+    width: Dimensions.get("window").width,
+    height: 300,
+  },
+  text: {
+    color: "white",
+  },
+});
